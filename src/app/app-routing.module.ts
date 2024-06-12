4926f83861701d6f98da7d8344d16a8c10c1dd8e
Andrea Castro <ap.castrocastellon@gmail.com>
2024-06-12 00:39:50 -0400

    base project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes), 
+    ReactiveFormsModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+
+
+}
