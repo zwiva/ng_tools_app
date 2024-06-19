32f527f36faa3d0d0fcf46d92a5d66cd3b2043a9
Andrea Castro <ap.castrocastellon@gmail.com>
2024-06-19 14:26:34 -0400

    avance vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'home2',
+  //   loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)
+  // },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes), 
+    RouterModule.forRoot(routes),
     ReactiveFormsModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
+export class AppRoutingModule {
 
 
 }
+ 
